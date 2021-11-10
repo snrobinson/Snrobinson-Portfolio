4b165c37537b63d03fc3e74928c91f42518e22e9
snrobinson <snrobinson94@gmail.com>
2021-11-09 21:35:19 -0800

    aws cert update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,14 @@
 //Get the button:
-mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    document.getElementById("myBtn").style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    document.getElementById("myBtn").style.display = "none";
   }
 }
 
