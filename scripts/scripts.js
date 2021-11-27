1c972f87de1e60e9401e5439ab2090dd43fa7ec4
snrobinson <snrobinson94@gmail.com>
2021-11-27 06:02:49 -0800

    wqds

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,9 @@
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -29,32 +31,31 @@ function myFunction() {
 }
 
 var slideIndex = 0;
-showSlides(slideIndex);
+document.querySelector("firstImgSS").style.display = "block";
+showSlides();
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
 }
 
 // var slideIndex = 0;
@@ -69,5 +70,23 @@ function showSlides(n) {
 //   slideIndex++;
 //   if (slideIndex > slides.length) {slideIndex = 1}
 //   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 20000); // Change image every 2 seconds
+//   setTimeout(showSlides, 30000); // Change image every 30 seconds
 // }
+
+// Home icon removal/add
+
+// let winWidth = window.innerWidth();
+// let winHeight = window.innerHeight;
+// let home = document.querySelector("#home-icon");
+
+// function resizeFunc() {
+//   if (winWidth >= 864) {
+//     home.classList.remove("#fa");
+//     home.classList.remove("#fa-home");
+//   } else {
+//     home.classList.add("#fa");
+//     home.classList.add("#fa-home");
+//   }
+// }
+
+// window.onresize(resizeFunc());
